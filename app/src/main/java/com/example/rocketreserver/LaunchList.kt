a8f6f58d9442799b520540accaf20f61e21a6fa6
BoD <BoD@JRAF.org>
2023-03-06 17:49:12 +0100

    Paginate the launch list

--- app/src/main/java/com/example/rocketreserver/LaunchList.kt
+++ app/src/main/java/com/example/rocketreserver/LaunchList.kt
@@ -3,9 +3,13 @@
 package com.example.rocketreserver
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
@@ -15,22 +19,34 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.api.Optional
 
 @Composable
 fun LaunchList(onLaunchClick: (launchId: String) -> Unit) {
+    var cursor: Optional<String?> by remember { mutableStateOf(Optional.absent()) }
+    var response: ApolloResponse<LaunchListQuery.Data>? by remember { mutableStateOf(null) }
     var launchList by remember { mutableStateOf(emptyList<LaunchListQuery.Launch>()) }
-    LaunchedEffect(Unit) {
-        val response = apolloClient.query(LaunchListQuery()).execute()
-        launchList = response.data?.launches?.launches?.filterNotNull() ?: emptyList()
+    LaunchedEffect(cursor) {
+        response = apolloClient.query(LaunchListQuery(cursor)).execute()
+        launchList = launchList + (response?.data?.launches?.launches?.filterNotNull() ?: emptyList())
     }
 
     LazyColumn {
         items(launchList) { launch ->
             LaunchItem(launch = launch, onClick = onLaunchClick)
+        }
+
+        item {
+            if (response?.data?.launches?.hasMore == true) {
+                LoadingItem()
+                cursor = Optional.present(response?.data?.launches?.cursor)
+            }
         }
     }
 }
@@ -58,4 +74,16 @@ private fun LaunchItem(launch: LaunchListQuery.Launch, onClick: (launchId: Strin
             )
         }
     )
+}
+
+@Composable
+private fun LoadingItem() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        CircularProgressIndicator()
+    }
 }
