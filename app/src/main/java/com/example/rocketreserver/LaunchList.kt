7ac0d484a2e902311b58d6f97e6d930cb69f6f16
BoD <BoD@JRAF.org>
2023-03-06 16:16:20 +0100

    Add LaunchList query

--- app/src/main/java/com/example/rocketreserver/LaunchList.kt
+++ app/src/main/java/com/example/rocketreserver/LaunchList.kt
@@ -9,11 +9,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -21,20 +27,26 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun LaunchList(onLaunchClick: (launchId: String) -> Unit) {
+    var launchList by remember { mutableStateOf(emptyList<LaunchListQuery.Launch>()) }
+    LaunchedEffect(Unit) {
+        val response = apolloClient.query(LaunchListQuery()).execute()
+        launchList = response.data?.launches?.launches?.filterNotNull() ?: emptyList()
+    }
+
     LazyColumn {
-        items(20) {
-            LaunchItem(launchId = it.toString(), onClick = onLaunchClick)
+        items(launchList) { launch ->
+            LaunchItem(launch = launch, onClick = onLaunchClick)
         }
     }
 }
 
 @Composable
-private fun LaunchItem(launchId: String, onClick: (launchId: String) -> Unit) {
+private fun LaunchItem(launch: LaunchListQuery.Launch, onClick: (launchId: String) -> Unit) {
     ListItem(
-        modifier = Modifier.clickable { onClick(launchId) },
+        modifier = Modifier.clickable { onClick(launch.id) },
         headlineText = {
             // Mission name
-            Text(text = "Launch $launchId")
+            Text(text = "Launch ${launch.id}")
         },
         supportingText = {
             // Site
