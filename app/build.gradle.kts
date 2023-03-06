4cd0cef8aac0d2043d56489c7ddee919c9d36e9c
BoD <BoD@JRAF.org>
2023-03-06 12:20:10 +0100

    Add Apollo Kotlin dependency and schema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.apollographql.apollo3").version("3.7.4")
 }
 
 android {
@@ -60,6 +61,9 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.security:security-crypto:1.1.0-alpha05")
+
+    implementation("com.apollographql.apollo3:apollo-runtime")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -67,4 +71,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.example.rocketreserver")
+    }
 }
