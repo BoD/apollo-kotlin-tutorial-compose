d6c79b1a7022d88230d6f0a280eba563961c5c59
BoD <BoD@JRAF.org>
2023-03-06 18:58:35 +0100

    Add LaunchDetails query

--- app/src/main/java/com/example/rocketreserver/LaunchDetails.kt
+++ app/src/main/java/com/example/rocketreserver/LaunchDetails.kt
@@ -1,6 +1,5 @@
 package com.example.rocketreserver
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,22 +14,35 @@ import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.apollographql.apollo3.api.ApolloResponse
 
 @Composable
 fun LaunchDetails(launchId: String) {
+    var response by remember { mutableStateOf<ApolloResponse<LaunchDetailsQuery.Data>?>(null) }
+    LaunchedEffect(Unit) {
+        response = apolloClient.query(LaunchDetailsQuery(launchId)).execute()
+    }
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             // Mission patch
-            Image(
+            AsyncImage(
                 modifier = Modifier.size(160.dp, 160.dp),
-                painter = painterResource(R.drawable.ic_placeholder),
+                model = response?.data?.launch?.mission?.missionPatch,
+                placeholder = painterResource(R.drawable.ic_placeholder),
+                error = painterResource(R.drawable.ic_placeholder),
                 contentDescription = "Mission patch"
             )
 
@@ -40,21 +52,21 @@ fun LaunchDetails(launchId: String) {
                 // Mission name
                 Text(
                     style = MaterialTheme.typography.headlineMedium,
-                    text = "Launch $launchId"
+                    text = response?.data?.launch?.mission?.name ?: ""
                 )
 
                 // Rocket name
                 Text(
                     modifier = Modifier.padding(top = 8.dp),
                     style = MaterialTheme.typography.headlineSmall,
-                    text = "Rocket name",
+                    text = response?.data?.launch?.rocket?.name?.let { "🚀 $it" } ?: "",
                 )
 
                 // Site
                 Text(
                     modifier = Modifier.padding(top = 8.dp),
                     style = MaterialTheme.typography.titleMedium,
-                    text = "Site..."
+                    text = response?.data?.launch?.site ?: "",
                 )
             }
         }
