3673d47ca38a978c6a310acc86ce46a00e64bcd1
BoD <BoD@JRAF.org>
2023-03-07 16:44:45 +0100

    Add TripsBooked subscription

--- app/src/main/java/com/example/rocketreserver/Apollo.kt
+++ app/src/main/java/com/example/rocketreserver/Apollo.kt
@@ -1,8 +1,10 @@
 package com.example.rocketreserver
 
 import android.content.Context
+import android.util.Log
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.network.okHttpClient
+import kotlinx.coroutines.delay
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
@@ -33,7 +35,13 @@ fun apolloClient(context: Context): ApolloClient {
 
     instance = ApolloClient.Builder()
         .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
+        .webSocketServerUrl("wss://apollo-fullstack-tutorial.herokuapp.com/graphql")
         .okHttpClient(okHttpClient)
+        .webSocketReopenWhen { throwable, attempt ->
+            Log.d("Apollo", "WebSocket got disconnected, reopening after a delay", throwable)
+            delay(attempt * 1000)
+            true
+        }
         .build()
 
     return instance!!
